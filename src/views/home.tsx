7e43f3436c2445e3f8adfb858d1d160d38bd1291
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-07-27 01:54:38 +0300

    Create a basic nav and a basic hero

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>home</div>
-  )
+    <div className="home-view">
+      <Nav />
+      <Hero />
+    </div>
+  );
 }
