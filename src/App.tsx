91926a101e19f962c5a2ba7391a50837ade7a842
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-07-26 11:55:14 +0300

    implement basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
-export default function App() {
-  return <h1 className="bg-stone-600">Hello World</h1>
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Register from "./views/Register";
+import Login from "./views/Login";
+import MyClubs from "./views/MyClubs";
+import MyMatches from "./views/MyMatches";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/myclubs"
+          element={<MyClubs />}
+        />
+        <Route
+          path="/mymatches"
+          element={<MyMatches />}
+        />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
