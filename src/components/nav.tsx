d83658281d508d42ad19e9a98b0b6af2b0857f8c
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-07-29 18:40:16 +0300

    Create Basic Navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,114 +1,70 @@
 import { useState } from "react";
+import logo from "../assets/logo-icon.png";
+import HamburgerIcon from "../assets/HamburgerIcon.tsx";
 
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <nav className="relative flex w-full items-center justify-between bg-slate-950 py-2 shadow-sm shadow-neutral-700/10 dark:bg-neutral-800 dark:shadow-black/30  lg:flex-wrap lg:justify-start">
-      {/* Container wrapper */}
-      <div className="flex w-full flex-wrap items-center justify-between px-6">
-        <div className="flex items-center">
-          {/* Hamburger Menu button */}
-          <button
-            className="block border-0 py-2 pr-2.5  hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-            type="button"
-            onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
-          >
-            <span className="[&>svg]:w-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="white"
-                className="h-7 w-7"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </button>
-          {/* Nav Logo */}
-          <a
-            className="text-red-300"
-            href="#!"
-          >
-            <span className="[&>svg]:ml-2 [&>svg]:mr-3 [&>svg]:h-6 [&>svg]:w-6 [&>svg]:lg:ml-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                />
-              </svg>
-            </span>
-          </a>
-        </div>
-        {/* Collapsible wrapper */}
-        <div
-          className={`${isOpen ? "flex" : "hidden"} flex-grow basis-[100%] items-center lg:!flex lg:basis-auto`}
-          id="navbarSupportedContentY"
-        >
-          {/* Left links */}
-          <ul className="mr-auto lg:flex lg:flex-row text-gray-50">
+    <nav className="bg-gray-950 h-fit flex text-gray-200 flex-wrap justify-between sm:justify-normal">
+      {/* Flex Child 1 */}
+      <div className="flex items-center justify-start w-1/2 sm:w-fit">
+        <a href="">
+          <img
+            src={logo}
+            alt=""
+            className="h-12"
+          />
+        </a>
+      </div>
+      {/* Flex Child 2 */}
+      <div className="hidden sm:block grow">
+        <ul className="flex items-center justify-center h-full gap-x-10">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">My Clubs</a>
+          </li>
+          <li>
+            <a href="">My Matches</a>
+          </li>
+          <li>
+            <a href="">Sign up</a>
+          </li>
+          <li>
+            <a href="">Log in</a>
+          </li>
+        </ul>
+      </div>
+      {/* Flex Child 3 */}
+      <div className="flex justify-end items-center sm:hidden w-1/2 sm:w-fit">
+        <button onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}>
+          <HamburgerIcon twStyles="h-8 fill-gray-200" />
+        </button>
+      </div>
+      {/* Flex Child 4 */}
+      {isOpen && (
+        <div className="block sm:hidden">
+          <ul className="flex flex-col items-start justify-center h-full">
             <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                Home
-              </a>
+              <a href="">Home</a>
             </li>
             <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                My Clubs
-              </a>
+              <a href="">My Clubs</a>
             </li>
             <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                My Matches
-              </a>
+              <a href="">My Matches</a>
             </li>
             <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                Login
-              </a>
+              <a href="">Sign up</a>
             </li>
             <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                Register
-              </a>
-            </li>
-            <li>
-              <a
-                className="block py-2 pr-2  transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
-                href="#!"
-              >
-                Language
-              </a>
+              <a href="">Log in</a>
             </li>
           </ul>
         </div>
-      </div>
+      )}
     </nav>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Allow other devices to access
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
