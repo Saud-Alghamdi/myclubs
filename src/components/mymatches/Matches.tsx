4d6f7b548c2a142117c7ee6fba9fccec64fefefa
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-22 22:50:07 +0300

    Decrease match card size

--- src/components/mymatches/Matches.tsx
+++ src/components/mymatches/Matches.tsx
@@ -6,25 +6,28 @@ export default function Matches() {
   return (
     <section>
       <header>
-        <h3 className="text-2xl font-bold md:text-3xl">
+        <h1 className="text-2xl font-bold md:text-3xl">
           Your Upcoming Matches:
-        </h3>
+        </h1>
       </header>
 
-      <div id="matches-wrapper" className="mx-auto mt-10 max-w-7xl space-y-10">
+      <div id="matches-wrapper" className="mx-auto mt-10 max-w-5xl space-y-10">
         {favoriteClubsMatches?.data?.map((match) => (
+          // Match Card - Flex //
           <div
             key={match.matchId}
             className="flex flex-wrap items-center gap-y-10 rounded-3xl bg-black bg-opacity-40 px-2 py-7"
           >
+            {/* Col 1 - Time &  Date */}
             <div className=" col flex w-full items-center justify-center gap-x-10 md:gap-x-20">
               <span className="text-md md:text-xl xl:text-2xl">
                 {match.date}
               </span>
             </div>
+            {/* Col 2 - First team */}
             <div className="col flex flex-grow flex-col items-center justify-center gap-y-3 ">
               <img
-                className="h-14 w-14 sm:h-20 sm:w-20 md:h-28 md:w-28 xl:h-40 xl:w-40 "
+                className="h-14 w-14 sm:h-20 sm:w-20 md:h-28 md:w-28  "
                 src={match.club1Logo}
                 alt={match.club1Name}
               />
@@ -32,12 +35,14 @@ export default function Matches() {
                 {match.club1Name}
               </span>
             </div>
+            {/* Col 3 - "VS" */}
             <div className="col flex flex-grow items-center justify-center">
               <span className="text-xl font-bold md:text-3xl">VS</span>
             </div>
+            {/* Col 4 - Second team */}
             <div className="col flex flex-grow flex-col items-center justify-center gap-y-3 ">
               <img
-                className="h-14 w-14 sm:h-20 sm:w-20 md:h-28 md:w-28 xl:h-40 xl:w-40"
+                className="h-14 w-14 sm:h-20 sm:w-20 md:h-28 md:w-28 "
                 src={match.club2Logo}
                 alt={match.club2Name}
               />
