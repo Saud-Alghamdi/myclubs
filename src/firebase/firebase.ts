3a604b185d790e02221eb747b8c34c017ab7abb4
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-23 13:55:53 +0300

    Change prod keys to dev keys in firebase config file

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_PROD_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_PROD_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROD_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_PROD_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_PROD_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_PROD_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_PROD_FIREBASE_DATABASE_URL,
+  apiKey: import.meta.env.VITE_DEV_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_DEV_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_DEV_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_DEV_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_DEV_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_DEV_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_DEV_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
