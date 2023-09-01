c566ad75810a6364d2e32ac2b5a5cac1eaed5b4d
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-09-01 13:43:01 +0300

    Change default port from 5173 to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Allow other devices to access
   server: {
-    host: "0.0.0.0",
-    port: 5173,
+    host: "0.0.0.0",  // Allow other devices to access
+    port: 3000,
   },
 });
